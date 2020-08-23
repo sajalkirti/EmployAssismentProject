e240367208f0da2f558e746887e2530a7be05580
sajalkirti <sajalkirti@gmail.com>
2020-08-23 16:16:19 +0530

    ngSwithch Added	:wq

--- EmployeeAssessment/src/app/app.component.ts
+++ EmployeeAssessment/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmployeeAssessment';
+  isAdmin:boolean=false;
+  optionSelect : string;
+  CheckValid(isvalid:boolean):void
+  {
+    this.isAdmin=isvalid;
+    this.optionSelect="";
+  }
 }
